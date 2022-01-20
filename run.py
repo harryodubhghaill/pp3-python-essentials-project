0d59c4b0edc99559eebc55d2cb4b68914d997540
harryodubhghaill <harryodubhghaill@hotmail.com>
2022-01-20 12:14:12 +0000

    Add start screen text and game loop

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+import os
+
+
 class Ship():
     """
     Defines Ship class with properties for placement and methods
@@ -21,7 +24,7 @@ class Ship():
 
 class Board():
     """
-    Defines Board class that takes parameters for size and number of 
+    Defines Board class that takes parameters for size and number of
     ships depending on difficulty. Takes name parameter to differentiate
     comp vs player board.
     """
@@ -37,3 +40,34 @@ class Board():
 
     def place_ship(self):
         print(self)
+
+
+# player_board = Board(9, player, 5)
+
+# player_board.print_board()
+
+# utility function to clear terminal
+def clear_term():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # check for os
+        command = 'cls'
+    os.system(command)
+
+
+def print_start():
+    start_screen = ("~"*50)+"\n"+("~"*50) + "\n~~~~~~     WELCOME TO BATTLESHIP ROYAL      ~~~~~~\n"+ "~~  OBJECTIVE: TOTAL DESTRUCTION OF YOUR ENEMY  ~~\n" + ("~"*50)+"\n"+("~"*50)
+    print(start_screen)
+
+
+def print_rules():
+    rules = "\n** HOW TO PLAY **\n" + "\nType our name into the field below\n" + "Select your Difficulty\n" + "Place your ships\n" + "Fire missiles at your opponents ships\n" + "Winner is first to clear all enemy ships\n"
+    print(rules)
+
+
+def game_loop():
+    clear_term()
+    print_start()
+    print_rules()
+
+
+game_loop()
